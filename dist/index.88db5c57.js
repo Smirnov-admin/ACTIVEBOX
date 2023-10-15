361c17554884eadfffa3590ad6459c6653860c8a
Kirill <64747295+Smirnov-admin@users.noreply.github.com>
2023-10-15 21:14:38 +0300

    Add files via upload

---
+++ dist/index.88db5c57.js
@@ -0,0 +1,10 @@
+let scrollHeader = document.querySelector(".header_element");
+window.onscroll = function() {
+    if (window.scrollY > 1) scrollHeader.classList.add("scrolled");
+    else scrollHeader.classList.remove("scrolled");
+};
+document.querySelector("#menu__burger").addEventListener("change", function(e) {
+    document.body.style.overflow = e.target.checked === true ? "hidden" : "";
+});
+
+//# sourceMappingURL=index.88db5c57.js.map
